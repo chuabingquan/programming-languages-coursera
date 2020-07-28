8510726b7df19fe7d5b4fa83ffa7824f5ad48f1a
Chua Bing Quan <chipneverdies@gmail.com>
2020-07-29 01:08:07 +0800

    Complete part B week 1 homework qns 4-7

--- Part-B/week-1/hw4.rkt
+++ Part-B/week-1/hw4.rkt
@@ -21,3 +21,30 @@
         [(null? xs) (error "list-nth-mod: empty list")]
         [#t (let ([i (remainder n (length xs))])
               (car (list-tail xs i)))]))
+
+; Q4.
+(define (stream-for-n-steps s n)
+  (if (= n 0)
+      null
+      (let ([pair (s)])
+        (cons (car pair) (stream-for-n-steps (cdr pair) (- n 1))))))
+
+; Q5.
+(define (funny-number-stream)
+  (letrec ([f (lambda (x)
+                (cons (if (= 0 (remainder x 5))
+                          (- 0 x)
+                          x)
+                      (lambda () (f (+ x 1))))
+                )])
+    (f 1)))
+
+; Q6.
+(define (dan-then-dog)
+  (letrec ([f (lambda (x y) (cons x (lambda () (f y x))))])
+    (f "dan.jpg" "dog.jpg")))
+
+; Q7.
+(define (stream-add-zero s)
+  (lambda () (let ([pair (s)])
+               (cons (cons 0 (car pair)) (stream-add-zero (cdr pair))))))
